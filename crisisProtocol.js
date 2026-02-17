75e3925fef0a966ab5ba1da14d04d27ab17bc6f3
cucarachaparlante <132963064+cucarachaparlante@users.noreply.github.com>
2026-02-17 12:28:59 -0600

    Create crisisProtocol.js

---
+++ crisisProtocol.js
@@ -0,0 +1,11 @@
+function activateCrisisProtocol() {
+    document.getElementById("crisisBanner").classList.remove("hidden");
+}
+
+function toggleResources() {
+    document.getElementById("resourcesPanel").classList.toggle("hidden");
+}
+
+function closeBanner() {
+    document.getElementById("crisisBanner").classList.add("hidden");
+}
